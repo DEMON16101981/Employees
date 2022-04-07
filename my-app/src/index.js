abc26a7209729fc8785a484595acda6a2c4801a2
DEMON16101981 <dm2263815@gmail.com>
2022-04-07 19:25:24 +0300

    commit

---
+++ my-app/src/index.js
@@ -0,0 +1,11 @@
+import * as ReactDOMClient from 'react-dom/client';
+import './index.css';
+import React from 'react';
+import App from './components/app/app';
+
+const container = document.getElementById('root');
+ 
+const root = ReactDOMClient.createRoot(container);
+ 
+
+root.render(<App/>);
